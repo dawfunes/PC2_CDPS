c29a9a565943e4b4513a557592403b04690d3cab
Victor Nieto Palacios <nietopalaciosvictor@gmail.com>
2025-01-25 20:49:33 +0100

    Funciona

--- build_parte3.py
+++ build_parte3.py
@@ -1,25 +1,25 @@
 import subprocess, os
 
 # Clonar repositorio de la app
-subprocess.call(['git', 'clone', 'https://github.com/CDPS-ETSIT/practica_creativa2.git'])
+subprocess.call(['git', 'clone', 'https://github.com/CDPS-ETSIT/practica_creativa2'])
 
 # Crear la imagen de ProductPage
-subprocess.call(['docker', 'build', '-t', 'product-page/g43', './ProductPage'])
+subprocess.call(['docker', 'build', '-t', 'productpage/43', './ProductPage'])
 print("Imagen de ProductPage creada")
 
 # Crear la imagen de Details
-subprocess.call(['docker', 'build', '-t', 'details/g43', './Details'])
+subprocess.call(['docker', 'build', '-t', 'details/43', './Details'])
 print("Imagen de Details creada")
 
 # Crear la imagen de Ratings
-subprocess.call(['docker', 'build', '-t', 'ratings/g43', './Ratings'])
+subprocess.call(['docker', 'build', '-t', 'ratings/43', './Ratinds'])
 print("Imagen de Ratings creada")
 
 # Crear la imagen de Reviews
 os.chdir('practica_creativa2/bookinfo/src/reviews')
 dir = os.getcwd()
 subprocess.call(['docker', 'run', '--rm', '-u', 'root', '-v', f'{dir}:/home/gradle/project', '-w', '/home/gradle/project', 'gradle:4.8.1', 'gradle', 'clean', 'build'])
-subprocess.call(['docker', 'build', '-t', 'reviews/g43', 'D:/Escritorio/PC2_CDPS/practica_creativa2/bookinfo/src/reviews/reviews-wlpcfg'])
+subprocess.call(['docker', 'build', '-t', 'reviews/43', './reviews-wlpcfg'])
 print("Imagen de Reviews creada")
 
 subprocess.call(['docker-compose', 'up'])
