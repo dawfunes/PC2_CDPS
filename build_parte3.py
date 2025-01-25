b640118e5aadb2f42dbc5fabc2165ffe10ceb0b2
Victor Nieto Palacios <nietopalaciosvictor@gmail.com>
2025-01-25 21:56:12 +0100

    Optimización Dockerfiles & Script

--- build_parte3.py
+++ build_parte3.py
@@ -3,23 +3,12 @@ import subprocess, os
 # Clonar repositorio de la app
 subprocess.call(['git', 'clone', 'https://github.com/CDPS-ETSIT/practica_creativa2'])
 
-# Crear la imagen de ProductPage
-subprocess.call(['docker', 'build', '-t', 'productpage/43', './ProductPage'])
-print("Imagen de ProductPage creada")
-
-# Crear la imagen de Details
-subprocess.call(['docker', 'build', '-t', 'details/43', './Details'])
-print("Imagen de Details creada")
-
-# Crear la imagen de Ratings
-subprocess.call(['docker', 'build', '-t', 'ratings/43', './Ratinds'])
-print("Imagen de Ratings creada")
-
-# Crear la imagen de Reviews
+# Crear la imagen de Reviews, ejecutando antes el comando requerido
 os.chdir('practica_creativa2/bookinfo/src/reviews')
 dir = os.getcwd()
 subprocess.call(['docker', 'run', '--rm', '-u', 'root', '-v', f'{dir}:/home/gradle/project', '-w', '/home/gradle/project', 'gradle:4.8.1', 'gradle', 'clean', 'build'])
 subprocess.call(['docker', 'build', '-t', 'reviews/43', './reviews-wlpcfg'])
-print("Imagen de Reviews creada")
 
+# Construir y levantar los contenedores con docker-compose
+subprocess.call(['docker-compose', 'build'])
 subprocess.call(['docker-compose', 'up'])
